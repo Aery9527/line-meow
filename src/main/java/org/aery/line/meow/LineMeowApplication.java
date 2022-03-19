1e94823ab7a27159108b46105e1fd55cbaea5bcc
nulll <nulllifeart@gmail.com>
2022-03-19 16:01:30 +0800

    init

---
+++ src/main/java/org/aery/line/meow/LineMeowApplication.java
@@ -0,0 +1,13 @@
+package org.aery.line.meow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineMeowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineMeowApplication.class, args);
+    }
+
+}
