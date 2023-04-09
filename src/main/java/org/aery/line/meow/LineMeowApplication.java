cd1b707518024789e7acf3ce5188089da4d09da8
AeryLin <nulllifeart@gmail.com>
2023-04-09 18:25:13 +0800

    modify gcp profile

--- src/main/java/org/aery/line/meow/LineMeowApplication.java
+++ src/main/java/org/aery/line/meow/LineMeowApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import java.util.Map;
-
 @SpringBootApplication
 @EnableConfigurationProperties
 public class LineMeowApplication {
 
     public static void main(String[] args) {
-//        Map<String, String> env = System.getenv();
-//        env.forEach((key, val) -> System.out.println(key + ":" + val));
         SpringApplication.run(LineMeowApplication.class, args);
     }
 
